76ab6c3d838d3cbf86920fc9354deda27cd1937a
Evgeniy Fateev <spaceflow@gmail.com>
2020-05-06 22:48:17 +0300

    feat(errors): add error serializer

---
+++ app/serializers/error_serializer.rb
@@ -0,0 +1,34 @@
+module ErrorSerializer
+  extend self
+
+  def from_message(error_messages, meta: {})
+    error_messages = Array.wrap(error_messages)
+    { errors: build_errors(error_messages, meta) }
+  end
+
+  def from_model(model)
+    { errors: build_model_errors(model.errors) }
+  end
+
+  private
+
+  def build_errors(error_messages, meta)
+    error_messages.map { |message| build_error(message, meta) }
+  end
+
+  def build_model_errors(errors)
+    errors.messages.map do |key, messages|
+      messages.map do |message|
+        error = build_error(message)
+        error[:source] = { pointer: "/data/attributes/#{key}" }
+        error
+      end
+    end.flatten
+  end
+
+  def build_error(message, meta = {})
+    error = { detail: message }
+    error[:meta] = meta if meta.present?
+    error
+  end
+end
---
+++ spec/serializers/error_serializer_spec.rb
@@ -0,0 +1,92 @@
+RSpec.describe ErrorSerializer do
+  subject { described_class }
+
+  describe 'from_message' do
+    context 'with single error message' do
+      let(:message) { 'Error message' }
+
+      it 'returns errors representation' do
+        expect(subject.from_message(message)).to eq(
+          errors: [
+            { detail: message }
+          ]
+        )
+      end
+    end
+
+    context 'with multiple error messages' do
+      let(:messages) { ['Error message 1', 'Error message 2'] }
+
+      it 'returns errors representation' do
+        expect(subject.from_message(messages)).to eq(
+          errors: [
+            { detail: messages[0] },
+            { detail: messages[1] }
+          ]
+        )
+      end
+    end
+
+    context 'with meta' do
+      let(:message) { 'Error message' }
+      let(:meta) { { level: 'error' } }
+
+      it 'returns errors representation' do
+        expect(subject.from_message(message, meta: meta)).to eq(
+          errors: [
+            {
+              detail: message,
+              meta: meta
+            }
+          ]
+        )
+      end
+    end
+  end
+
+  describe 'from_model' do
+    let(:model) do
+      Class.new do
+        include ActiveModel::Model
+
+        attr_accessor :blue, :green
+
+        validates :blue, :green, presence: true
+        validates :green, inclusion: { in: [1] }
+
+        def self.name
+          'Model'
+        end
+      end.new
+    end
+
+    before do
+      model.validate
+    end
+
+    it 'returns errors representation' do
+      expect(subject.from_model(model)).to eq(
+        errors: [
+          {
+            detail: %(не может быть пустым),
+            source: {
+              pointer: '/data/attributes/blue'
+            }
+          },
+          {
+            detail: %(не может быть пустым),
+            source: {
+              pointer: '/data/attributes/green'
+            }
+          },
+          {
+            detail: %(имеет непредусмотренное значение),
+            source: {
+              pointer: '/data/attributes/green'
+            }
+          }
+        ]
+      )
+    end
+  end
+end
